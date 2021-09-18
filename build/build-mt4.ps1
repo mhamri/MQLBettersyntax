2fe28c5f30f3e59550b8b12bd78af916b61332cc
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-09-18 18:32:40 +0800

    add a build script and task.json for vscode

---
+++ build/build-mt4.ps1
@@ -0,0 +1,110 @@
+#gets the File To Compile as an external parameter... Defaults to a Test file...
+Param( $MetaTraderExecutable, $FileToCompile, $IncludeFolder )
+
+#cleans the terminal screen and sets the log file name...
+Clear-Host
+
+Function Test-IsFileLocked {
+    [cmdletbinding()]
+    Param (
+        [parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+        [Alias('FullName', 'PSPath')]
+        [string[]]$Path
+    )
+    Process {
+        ForEach ($Item in $Path) {
+            #Ensure this is a full path
+            $Item = Convert-Path $Item
+            #Verify that this is a file and not a directory
+            If ([System.IO.File]::Exists($Item)) {
+                Try {
+                    $FileStream = [System.IO.File]::Open($Item, 'Open', 'Write')
+                    $FileStream.Close()
+                    $FileStream.Dispose()
+                    $IsLocked = $False
+                }
+                Catch [System.UnauthorizedAccessException] {
+                    $IsLocked = 'AccessDenied'
+                }
+                Catch {
+                    $IsLocked = $True
+                }
+                [pscustomobject]@{
+                    File     = $Item
+                    IsLocked = $IsLocked
+                }
+            }
+        }
+    }
+}
+
+$LogFile = $FileToCompile + ".log"
+if (Test-Path $LogFile) {
+    Remove-Item $LogFile -Force | Out-Null
+}
+
+#before continue check if the Compile File has any spaces in it...
+# if ($FileToCompile.Contains(" ")) {
+#     "";"";
+#     Write-Host "ERROR!  Impossible to Compile! Your Filename or Path contains SPACES!" -ForegroundColor Red;
+#     "";
+#     Write-Host $FileToCompile -ForegroundColor Red;
+#     "";"";
+#     return;
+# }
+
+#fires up the Metaeditor compiler...
+& "$MetaTraderExecutable" /compile:"$FileToCompile" /log:"$LogFile" /inc:"$IncludeFolder" 
+$counter = 10;
+while (!(Test-Path $LogFile) -and $counter -lt 100) { 
+    Start-Sleep 1
+    $counter += 1
+}
+#get some clean real state and tells the user what is being compiled (just the file name, no path)...
+
+$JustTheFileName = Split-Path $FileToCompile -Leaf
+Write-Host "Compiling........: $JustTheFileName`n"
+
+while ((Test-IsFileLocked $LogFile).IsLocked ) {
+    Start-Sleep 1
+}
+
+#reads the log file. Eliminates the blank lines. Skip the first line because it is useless.
+$Log = Get-Content $LogFile 
+
+# | Where-Object { $_ -ne "" } | Select-Object -Skip 1
+
+#Green color for successful Compilation. Otherwise (error/warning), Red!
+$WhichColor = "Red"
+$Log | ForEach-Object { 
+    if ($_ -match ("0 error.*, 0 warning.*")) { $WhichColor = "Green" } 
+}
+
+#runs through all the log lines...
+$Log | ForEach-Object {
+    #ignores the ": information: error generating code" line when ME was successful
+    if (-Not $_.Contains("information:")) {
+        #common log line... just print it...
+        Write-Host $_ -ForegroundColor $WhichColor
+    }
+}
+
+# Write-Host $WhichColor
+
+$terminalPath = Split-Path $MetaTraderExecutable 
+Write-Host $terminalPath
+#get the MT Terminal back if all went well...
+if ( $WhichColor -eq "Green") { 
+    Write-Host "`nBuild was successful. Restarting terminal ..." -ForegroundColor $WhichColor
+    #first of all, kill MT Terminal (if running)... otherwise it will not see the new compiled version of the code...
+    $runningTerminal = Get-Process -Name "*terminal*"  | where-object { $_.path -match $([regex]::escape($terminalPath)) } | Select-Object  Id, path
+    # Stop-Process $runningTerminal.Id
+
+    & $runningTerminal.Path
+
+}
+else {
+    Write-Host "`nBuild Failed." -ForegroundColor $WhichColor
+}
+
+Remove-Item $LogFile | Out-Null
